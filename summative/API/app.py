2ad8e18cc7c9d9df88e6a3afc69cafeeb51d8de7
tomunizua <your.email@example.com>
2024-11-24 13:20:15 +0200

    api

--- summative/API/app.py
+++ summative/API/app.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 from fastapi.middleware.cors import CORSMiddleware
 import joblib
 import numpy as np
 import uvicorn
+import datetime
 
 # Define FastAPI app
 app = FastAPI(
     title="Demand Prediction API",
-    description="An API to predict demand for food deliveries using a Decision trees model.",
+    description="An API to predict demand for food deliveries using a Decision Tree model.",
     version="1.0"
 )
 
@@ -26,59 +27,101 @@ model = joblib.load('best_model.pkl')
 
 # Define the input data model using Pydantic
 class PredictionInput(BaseModel):
-    order_date: str = Field(..., pattern=r'^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$')
+    Weatherconditions: str 
+    Road_traffic_density: str 
+    Type_of_order: str 
+    Festival: str = Field(..., regex="^(no|yes)$")
+    City: str 
     hour: int = Field(..., ge=0, le=23)
-    day_of_week: str
-    is_holiday: bool
-    weather_conditions: str
+    day_of_week: str = Field(..., regex="^(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)$")
+    order_date: int = Field(..., regex=r'^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$')
 
     class Config:
         schema_extra = {
             "example": {
-                "order_date": "2024-11-23",
+                "Weatherconditions": "sunny",
+                "Road_traffic_density": "medium",
+                "Type_of_order": "Snack",
+                "Festival": "no",
+                "City": "Urban",
                 "hour": 14,
                 "day_of_week": "Friday",
-                "is_holiday": False,
-                "weather_conditions": "Sunny"
+                "order_date": 2024-11-23
             }
         }
 
-# Mapping from day names to their respective numbers
-day_of_week_map = {
-    'monday': 0,
-    'tuesday': 1,
-    'wednesday': 2,
-    'thursday': 3,
-    'friday': 4,
-    'saturday': 5,
-    'sunday': 6
-}
+    @model_validator('order_date', pre=True) 
+    def validate_order_date(cls, v): 
+        try: 
+            datetime.datetime.strptime(v, '%Y-%m-%d') 
+        except ValueError: 
+            raise ValueError('Order date must be in YYYY-MM-DD format') 
+        return v
 
+# Mapping dictionaries
 weather_conditions_map = {
-    'sunny': 0,
-    'cloudy': 1,
-    'rainy': 2,
-    'snowy': 3
+    'Cloudy': 0,
+    'Fog': 1,
+    'Sandstorms': 3,
+    'Stormy': 4,
+    'Sunny': 5,
+    'Windy': 6,
 }
 
+road_traffic_density_map = {
+    'High': 0,
+    'Jam': 1,
+    'Low': 2,
+    'Medium': 3
+}
 
-def is_weekend(day_of_week: str) -> bool:
-    day_of_week_num = day_of_week_map[day_of_week.lower()]
-    return day_of_week_num in [5, 6]
+type_of_order_map = {
+    'Drinks': 0,
+    'Meal': 1,
+    'Snack': 2
+}
+
+festival_map = {
+    'no': 1,
+    'yes': 2
+}
+
+city_map = {
+    'Metropolitian': 0,
+    'Semi-Urban': 2,
+    'Urban': 3
+}
+
+day_of_week_map = {
+    'Monday': 0,
+    'Tuesday': 1,
+    'Wednesday': 2,
+    'Thursday': 3,
+    'Friday': 4,
+    'Saturday': 5,
+    'Sunday': 6
+}
 
 @app.post("/predict")
 def predict(input_data: PredictionInput):
     try:
         input_dict = input_data.dict()
+        weather_conditions_num = weather_conditions_map[input_dict["Weatherconditions"]]
+        road_traffic_density_num = road_traffic_density_map[input_dict["Road_traffic_density"]]
+        type_of_order_num = type_of_order_map[input_dict["Type_of_order"]]
+        festival_num = festival_map[input_dict["Festival"].lower()]
+        city_num = city_map[input_dict["City"]]
         day_of_week_num = day_of_week_map[input_dict["day_of_week"].lower()]
-        weekend = is_weekend(input_dict["day_of_week"])
-        weather_conditions_num = weather_conditions_map[input_dict["weather_conditions"].lower()]
+        
         features = [
+            weather_conditions_num,
+            road_traffic_density_num,
+            type_of_order_num,
+            festival_num,
+            city_num,
             input_dict["hour"],
             day_of_week_num,
-            int(weekend),
-            int(input_dict["is_holiday"]),
-            weather_conditions_num
+            int(input_dict["order_date"].split('-')[2])
         ]
 
         features_array = np.array(features).reshape(1, -1)
